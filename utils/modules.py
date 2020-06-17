2e5590d91e467f402a113c3a4db33b50018a2c64
yjh0410 <1394571815@qq.com>
2020-06-17 10:02:01 +0800

    add RFBblocks

--- utils/modules.py
+++ utils/modules.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torch.nn.functional as F
 
-
 class Conv2d(nn.Module):
     def __init__(self, in_channels, out_channels, ksize, padding=0, stride=1, dilation=1, leakyReLU=False):
         super(Conv2d, self).__init__()
@@ -78,3 +77,37 @@ class ASPP(nn.Module):
         x = torch.cat([x_1, x_2_1, x_2_2, x_2_3], dim=1)
 
         return self.fusion(x)
+
+class RFBblock(nn.Module):
+    def __init__(self, in_ch, residual=False):
+        super(RFBblock, self).__init__()
+        inter_c = in_ch // 4
+        self.branch_0 = nn.Sequential(
+                    nn.Conv2d(in_channels=in_ch, out_channels=inter_c, kernel_size=1, stride=1, padding=0),
+                    )
+        self.branch_1 = nn.Sequential(
+                    nn.Conv2d(in_channels=in_ch, out_channels=inter_c, kernel_size=1, stride=1, padding=0),
+                    nn.Conv2d(in_channels=inter_c, out_channels=inter_c, kernel_size=3, stride=1, padding=1)
+                    )
+        self.branch_2 = nn.Sequential(
+                    nn.Conv2d(in_channels=in_ch, out_channels=inter_c, kernel_size=1, stride=1, padding=0),
+                    nn.Conv2d(in_channels=inter_c, out_channels=inter_c, kernel_size=3, stride=1, padding=1),
+                    nn.Conv2d(in_channels=inter_c, out_channels=inter_c, kernel_size=3, stride=1, dilation=2, padding=2)
+                    )
+        self.branch_3 = nn.Sequential(
+                    nn.Conv2d(in_channels=in_ch, out_channels=inter_c, kernel_size=1, stride=1, padding=0),
+                    nn.Conv2d(in_channels=inter_c, out_channels=inter_c, kernel_size=5, stride=1, padding=2),
+                    nn.Conv2d(in_channels=inter_c, out_channels=inter_c, kernel_size=3, stride=1, dilation=3, padding=3)
+                    )
+        self.residual= residual
+
+    def forward(self,x):
+        x_0 = self.branch_0(x)
+        x_1 = self.branch_1(x)
+        x_2 = self.branch_2(x)
+        x_3 = self.branch_3(x)  
+        out = torch.cat((x_0,x_1,x_2,x_3),1)
+        if self.residual:
+            out +=x 
+        return out
+
